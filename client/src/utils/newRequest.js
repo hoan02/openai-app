5b7eaafc405b314d0ea22fffe7504edb87cd58b5
hoanit02 <hoanvipboi1@gmail.com>
2023-03-19 14:22:32 +0700

    fix base URL client

--- client/src/utils/newRequest.js
+++ client/src/utils/newRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const newRequest = axios.create({
-  baseURL: "https://openai-api-sever.onrender.com/api/",
+  baseURL: "https://open-ai-sever.onrender.com/api",
   withCredentials: true,
   headers: {
     'content-type': 'application/json',
