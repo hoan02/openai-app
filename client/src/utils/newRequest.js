31d8194c2ce27cefb2e8c73b8cba8d8f91f1c950
hoanit02 <hoanvipboi1@gmail.com>
2023-03-24 00:08:01 +0700

    setting cookie

--- client/src/utils/newRequest.js
+++ client/src/utils/newRequest.js
@@ -1,10 +1,7 @@
 import axios from "axios";
-import * as dotenv from "dotenv";
-
-dotenv.config()
 
 const newRequest = axios.create({
-  baseURL: process.env.BASE_URL_SERVER,
+  baseURL: "https://server-ymt3.onrender.com/api/",
   headers: { 'Content-Type': 'application/json' },
   withCredentials: true,
 });
