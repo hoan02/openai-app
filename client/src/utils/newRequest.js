1aee3b6e53bf62ef04d41e738a243ecddf6ab778
hoanit02 <hoanvipboi1@gmail.com>
2023-03-23 22:21:58 +0700

    fix base url api

--- client/src/utils/newRequest.js
+++ client/src/utils/newRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const newRequest = axios.create({
-  baseURL: "http://localhost:8080/api/",
+  baseURL: "https://server-ymt3.onrender.com/api/",
   withCredentials: true,
 });
 
