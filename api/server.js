652f0264514beb73ce14401ded98c0e51f3f1a19
hoanit02 <hoanvipboi1@gmail.com>
2023-03-23 22:46:44 +0700

    fixx

--- api/server.js
+++ api/server.js
@@ -35,7 +35,7 @@ const connectDB = async () => {
   }
 };
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://openai-q13r.onrender.com", credentials: true }));
 app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
 
--- client/src/utils/newRequest.js
+++ client/src/utils/newRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const newRequest = axios.create({
-  baseURL: "https://server-ymt3.onrender.com/api/",
+  baseURL: "https://server-ymt3.onrender.com/api",
   withCredentials: true,
 });
 
