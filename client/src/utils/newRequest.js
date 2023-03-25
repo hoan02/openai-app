7a24e15b94dead33edda753577941e94ea669f1a
hoanit02 <hoanvipboi1@gmail.com>
2023-03-26 04:58:40 +0700

    push

--- client/src/utils/newRequest.js
+++ client/src/utils/newRequest.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const newRequest = axios.create({
-  // baseURL: "https://server-ymt3.onrender.com/api/",
-  baseURL: "http://localhost:8080/api/",
+  baseURL: "https://server-ymt3.onrender.com/api/",
+  // baseURL: "http://localhost:8080/api/",
   headers: { 'Content-Type': 'application/json' },
   withCredentials: true,
 });
